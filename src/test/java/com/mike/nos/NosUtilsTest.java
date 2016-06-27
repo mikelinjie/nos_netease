590f09eca3d0e327db062433dfa8c430b4bae1c6
林杰 <hzlinjie1@corp.netease.com>
2016-06-27 19:19:13 +0800

    test demo

---
+++ src/test/java/com/mike/nos/NosUtilsTest.java
@@ -0,0 +1,24 @@
+package com.mike.nos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NosUtilsTest {
+
+    @Test
+    public void testInitInputStream() {
+        System.out.println("testInitInputStream");
+    }
+
+    @Test
+    public void testInitStringString() {
+        System.out.println("testInitStringString");
+    }
+
+    @Test
+    public void testUploadFileStringStringFile() {
+        System.out.println("testUploadFileStringStringFile");
+    }
+
+}
